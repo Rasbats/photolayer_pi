ef6b953d5c41171f295cbd13f7e8b4c18807f658
Rasbats <miker@members.swis.net>
2020-07-20 17:27:01 +0100

    v3.1.12.0

--- ci/circleci-build-mingw.sh
+++ ci/circleci-build-mingw.sh
@@ -6,9 +6,6 @@
 
 set -xe
    
-su apt-get install libgeotiff-dev
-su apt-get install libtiff-dev  
-
 su -c "dnf install -q -y sudo dnf-plugins-core"
 sudo dnf builddep -y mingw/fedora/opencpn-deps.spec
 rm -rf build; mkdir build; cd build
