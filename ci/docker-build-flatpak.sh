3cea36e3792c7967d02153e26478d5a74236349f
Rasbats <miker@members.swis.net>
2021-01-03 20:19:31 +0000

    update ci using testplugin files

--- ci/docker-build-flatpak.sh
+++ ci/docker-build-flatpak.sh
@@ -8,10 +8,6 @@ set -xe
 
 df -h
 cd $TOPDIR
-
-su -c "dnf install -y libgeotiff-devel"
-su -c "dnf install -y libtiff-devel" 
-	
 su -c "dnf install -y sudo cmake gcc-c++ flatpak-builder flatpak make tar"
 flatpak remote-add --user --if-not-exists flathub \
     https://flathub.org/repo/flathub.flatpakrepo
@@ -20,6 +16,7 @@ if [ "" = "$ocpnfound" ]; then
    flatpak install --user  -y \
        http://opencpn.duckdns.org/opencpn/opencpn.flatpakref
 fi
+
 flatpak install --user -y  flathub org.freedesktop.Sdk//18.08
 #rm -rf flatpak/.flatpak-builder && rm -rf build && mkdir build && cd build
 rm -rf build && mkdir build && cd build
