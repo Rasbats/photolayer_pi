bafa33f9bffee398acf306e16663582d4275d3dc
Alec Leamas <leamas.alec@nowhere.nt>
2021-01-09 13:24:00 +0100

    libgeotiff: Refactor CmakeLists into sane parts.

--- libs/libgeotiff/cmake/FindGeoTIFF.cmake
+++ libs/libgeotiff/cmake/FindGeoTIFF.cmake
@@ -15,6 +15,9 @@ find_path(GEOTIFF_INCLUDE_DIR geotiff.h PATH_SUFFIXES geotiff libgeotiff)
 set(GEOTIFF_NAMES ${GEOTIFF_NAMES} geotiff_i geotiff libgeotiff_i libgeotiff)
 find_library(GEOTIFF_LIBRARY NAMES ${GEOTIFF_NAMES})
 
+set(GEOTIFF_LIBRARIES ${GEOTIFF_LIBRARY})
+set(GEOTIFF_INCLUDE_DIRS ${GEOTIFF_INCLUDE_DIR})
+
 mark_as_advanced(GEOTIFF_INCLUDE_DIR GEOTIFF_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
