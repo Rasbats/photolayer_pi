7eb587cd590566c827b1d32248920d745cf2ef5a
Alec Leamas <leamas.alec@nowhere.nt>
2021-01-13 22:46:48 +0100

    libgeotiff: Download sources on build.

--- libs/libgeotiff/LibGeotiff.cmake
+++ libs/libgeotiff/LibGeotiff.cmake
@@ -30,7 +30,11 @@ set(CMAKE_POSITION_INDEPENDENT_CODE "ON")
 
 ExternalProject_Add(
   EXT_GEOTIFF
+  URL http://download.osgeo.org/geotiff/libgeotiff/libgeotiff-1.6.0.tar.gz
+  URL_HASH
+    SHA256=9311017e5284cffb86f2c7b7a9df1fb5ebcdc61c30468fb2e6bca36e4272ebca
   SOURCE_DIR ${PROJECT_SOURCE_DIR}/libgeotiff-1.6.0
+  DOWNLOAD_DIR ${PROJECT_SOURCE_DIR}/cache
   CONFIGURE_COMMAND autoreconf -fi && ./configure CFLAGS=-fPIC
     --with-proj=${_install_root}
     --prefix=${_install_root}
--- libs/libgeotiff/Proj.cmake
+++ libs/libgeotiff/Proj.cmake
@@ -26,6 +26,10 @@ endif ()
 
 ExternalProject_Add(
   EXT_PROJ
+  URL https://github.com/OSGeo/PROJ/releases/download/7.2.1/proj-7.2.1.tar.gz
+  URL_HASH
+    SHA256=b384f42e5fb9c6d01fe5fa4d31da2e91329668863a684f97be5d4760dbbf0a14
+  DOWNLOAD_DIR    ${PROJECT_SOURCE_DIR}/cache
   SOURCE_DIR      ${PROJECT_SOURCE_DIR}/proj-7.2.1
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND 
