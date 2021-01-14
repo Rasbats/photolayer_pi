73e54da0712cef251c83287cdc7620ee9b829a10
Alec Leamas <leamas.alec@nowhere.nt>
2021-01-14 20:31:48 +0100

    libgeotiff: Clean up windows handling, bugfixes.

--- libs/libgeotiff/GeoTiff-msvc.cmake
+++ libs/libgeotiff/GeoTiff-msvc.cmake
@@ -1,16 +1,21 @@
 #
 # Build the pre-built libgeotiff msvc binary. Exports:
 #
-# GEOTIFF_LIBRARIES     The static library
-# GEOTIFF_INCLUDE_DIRS  Public header files
+# msvc::geotiff         Interface library carrying headers and library
 
 set(CMAKE_POSITION_INDEPENDENT_CODE "ON")
 
 file(GLOB _geotiff_dir "${CMAKE_CURRENT_SOURCE_DIR}/libgeotiff-*-msvc")
-add_library(GEOTIFF_LIBRARIES STATIC IMPORTED)
+
+add_library(_msvc_geotiff INTERFACE)
+
+add_library(GEOTIFF_LIB STATIC IMPORTED)
 set_property(
-  TARGET GEOTIFF_LIBRARIES
+  TARGET GEOTIFF_LIB
   PROPERTY IMPORTED_LOCATION ${_geotiff_dir}/geotiff.lib
 )
-message(STATUS "GEOTIFF LIB LOCATION: ${_geotiff_dir}/geotiff.lib")
-set(GEOTIFF_INCLUDE_DIRS ${_geotiff_dir}/include ${_geotiff_dir}/libxtiff)
+target_link_libraries(_msvc_geotiff INTERFACE GEOTIFF_LIB)
+target_include_directories(_msvc_geotiff
+  INTERFACE ${_geotiff_dir}/include ${_geotiff_dir}/libxtiff
+)
+add_library(msvc::geotiff ALIAS _msvc_geotiff)
