1091230fc05fb8c85b4213ba9bac85f34def20b5
Alec Leamas <leamas.alec@nowhere.nt>
2021-01-14 18:57:56 +0100

    libgeotiff: Fix windows FTBFS typo (well, almost).

--- libs/libgeotiff/GeoTiff-msvc.cmake
+++ libs/libgeotiff/GeoTiff-msvc.cmake
@@ -1,15 +1,16 @@
 #
 # Build the pre-built libgeotiff msvc binary. Exports:
 #
-# GEOTIFF_MSVC_LIBRARIES     The static library
-# GEOTIFF_MSVC_INCLUDE_DIRS  Public header files
+# GEOTIFF_LIBRARIES     The static library
+# GEOTIFF_INCLUDE_DIRS  Public header files
 
 set(CMAKE_POSITION_INDEPENDENT_CODE "ON")
 
 file(GLOB _geotiff_dir "${CMAKE_CURRENT_SOURCE_DIR}/libgeotiff-*-msvc")
 add_library(GEOTIFF_LIBRARIES STATIC IMPORTED)
 set_property(
-  TARGET GEOTIFF_MSVC_LIBRARIES
+  TARGET GEOTIFF_LIBRARIES
   PROPERTY IMPORTED_LOCATION ${_geotiff_dir}/geotiff.lib
 )
-set(GEOTIFF_INCLUDE_DIRS ${geotiff_dir}/include)
+message(STATUS "GEOTIFF LIB LOCATION: ${_geotiff_dir}/geotiff.lib")
+set(GEOTIFF_INCLUDE_DIRS ${_geotiff_dir}/include ${_geotiff_dir}/libxtiff)
