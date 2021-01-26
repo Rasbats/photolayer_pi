98c774fa42c103220fc29ffbd06b8b58afa9182b
Rasbats <miker@members.swis.net>
2021-01-26 08:08:01 +0000

    Revert "v3.3.3"
    This reverts commit c7c4068d25cb4defeac9bee21ce067429fb6a8cb.

--- ci/circleci-build-debian.sh
+++ ci/circleci-build-debian.sh
@@ -7,7 +7,7 @@ set -xe
 sudo apt -qq update || apt update
 sudo apt-get -q install  devscripts equivs
 
-rm -rf build && mkdir  build
+mkdir  build
 cd build
 sudo mk-build-deps -ir ../build-deps/control
 sudo apt-get -q --allow-unauthenticated install -f
@@ -17,18 +17,13 @@ if [ -n "$BUILD_GTK3" ]; then
         /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
 fi
 
-cmake \
-  -DCMAKE_BUILD_TYPE=Release .. \
-  -DCMAKE_INSTALL_PREFIX= \
-  -DGEOTIFF_INSTALL_PREFIX="$HOME/.local" \
-  -DUSE_SYSTEM_GEOTIFF:BOOL="ON"
-
-make -j ${MAX_JOBS:-$(nproc)} VERBOSE=1 tarball
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j $(nproc) VERBOSE=1 tarball
 
 sudo apt-get install \
     python3-pip python3-setuptools python3-dev python3-wheel \
     build-essential libssl-dev libffi-dev 
 
 python3 -m pip install --user --upgrade pip
-python3 -m pip install -I --user -q cloudsmith-cli
-python3 -m pip install -I --user -q cryptography
+python3 -m pip install --user -q cloudsmith-cli
+python3 -m pip install --user -q cryptography
