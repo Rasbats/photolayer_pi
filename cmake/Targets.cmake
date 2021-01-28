0dc2d19f652f0d3aa8b5cf2bb760d8d9a246c9b0
Rasbats <miker@members.swis.net>
2021-01-28 16:34:02 +0000

    Revert "Update Targets.cmake"
    This reverts commit 092fa8869466a2d653850c6af72579da28518f54.

--- cmake/Targets.cmake
+++ cmake/Targets.cmake
@@ -15,10 +15,22 @@ if (WIN32)
   endif ()
 endif ()
 
+# Set up _parallel_cmake_opt
+if (${CMAKE_MAJOR_VERSION} LESS 3 OR ${CMAKE_MINOR_VERSION} LESS 12)
+  set(_parallel_cmake_opt "")
+else ()
+  set(_parallel_cmake_opt "--parallel")
+endif ()
+
 # Set up _build_cmd
-set(_build_cmd
-  cmake --build ${CMAKE_BINARY_DIR} --parallel --config $<CONFIG>
-)
+if (${CMAKE_MAJOR_VERSION} LESS 3 OR ${CMAKE_MINOR_VERSION} LESS 10)
+  set(_build_cmd make -j2)
+else ()
+  set(_build_cmd
+    cmake --build ${CMAKE_BINARY_DIR} ${_parallel_cmake_opt}
+    --config $<CONFIG>
+  )
+endif ()
 
 # Set up _build_target_cmd and _install_cmd
 if (${CMAKE_MAJOR_VERSION} LESS 3 OR ${CMAKE_MINOR_VERSION} LESS 16)
@@ -29,6 +41,14 @@ else ()
       cmake --build ${CMAKE_BINARY_DIR} --config $<CONFIG> --target
   )
   set(_install_cmd cmake --install ${CMAKE_BINARY_DIR} --config $<CONFIG>)
+endif ()
+
+
+# Command to compute sha256 checksum
+if (${CMAKE_MAJOR_VERSION} LESS 3 OR ${CMAKE_MINOR_VERSION} LESS 10)
+  set(_cs_command "${pkg_python} ${PROJECT_SOURCE_DIR}/ci/checksum.py")
+else ()
+  set(_cs_command "cmake -E sha256sum" )
 endif ()
 
 # Command to remove directory
@@ -43,7 +63,7 @@ endif ()
 #
 set(_cs_script "
   execute_process(
-    COMMAND  cmake -E sha256sum ${CMAKE_BINARY_DIR}/${pkg_tarname}.tar.gz
+    COMMAND ${_cs_command}  ${CMAKE_BINARY_DIR}/${pkg_tarname}.tar.gz
     OUTPUT_FILE ${CMAKE_BINARY_DIR}/${pkg_tarname}.sha256
   )
   file(READ ${CMAKE_BINARY_DIR}/${pkg_tarname}.sha256 _SHA256)
