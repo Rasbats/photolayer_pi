2c65a59acbcadeb6be06e62ad9a9f9d7b9225cd0
Rasbats <miker@members.swis.net>
2021-03-05 07:21:51 +0000

    Revert "align shipdriver"
    This reverts commit 0986703ffba4b5cab9be90e46ba4d72f3738ac63.

--- ci/circleci-build-debian.sh
+++ ci/circleci-build-debian.sh
@@ -5,19 +5,11 @@
 #
 set -xe
 sudo apt -qq update || apt update
-sudo apt-get -qq install devscripts equivs software-properties-common
+sudo apt-get -q install  devscripts equivs
 
-if [ -n  "$USE_DEADSNAKES_PY37" ]; then
-    sudo add-apt-repository -y ppa:deadsnakes/ppa
-    sudo apt -qq update
-    sudo  apt-get -q install  python3.7
-    for py in $(ls /usr/bin/python3.[0-9]); do
-        sudo update-alternatives --install /usr/bin/python3 python3 $py 1
-    done
-    sudo update-alternatives --set python3 /usr/bin/python3.7
-fi
-
-sudo mk-build-deps -ir build-deps/control
+rm -rf build && mkdir  build
+cd build
+sudo mk-build-deps -ir ../build-deps/control
 sudo apt-get -q --allow-unauthenticated install -f
 
 if [ -n "$BUILD_GTK3" ]; then
@@ -25,14 +17,19 @@ if [ -n "$BUILD_GTK3" ]; then
         /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
 fi
 
-sudo apt install -q \
+cmake \
+  -DCMAKE_BUILD_TYPE=Release .. \
+  -DCMAKE_INSTALL_PREFIX= \
+  -DGEOTIFF_INSTALL_PREFIX="$HOME/.local" \
+  -DUSE_SYSTEM_GEOTIFF:BOOL="ON"
+
+make -j ${MAX_JOBS:-$(nproc)} VERBOSE=1 tarball
+
+sudo apt-get install \
     python3-pip python3-setuptools python3-dev python3-wheel \
     build-essential libssl-dev libffi-dev 
 
-python3 -m pip install --user --upgrade -q setuptools wheel pip
-python3 -m pip install --user -q cloudsmith-cli cryptography cmake
-
-mkdir  build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j $(nproc) VERBOSE=1 tarball
+# Latest pip 21.0.0 is broken:
+python3 -m pip install --force-reinstall pip==20.3.4
+python3 -m pip install -I --user -q cloudsmith-cli
+python3 -m pip install -I --user -q cryptography
--- ci/generic-build-raspbian-armhf.sh
+++ ci/generic-build-raspbian-armhf.sh
@@ -7,63 +7,72 @@
 
 set -xe
 
-# Create build script
-#
-if [ -n "$TRAVIS_BUILD_DIR" ]; then
-    ci_source="$TRAVIS_BUILD_DIR"
-elif [ -d ~/project ]; then
-    ci_source=~/project
-else
-    ci_source="$(pwd)"
-fi
+# Start up the docker image
+DOCK_SOCK="unix:///var/run/docker.sock"
+echo "DOCKER_OPTS=\"-H tcp://127.0.0.1:2375 -H $DOCK_SOCK -s devicemapper\"" \
+    | sudo tee /etc/default/docker > /dev/null
+sudo service docker restart
+sleep 5;
 
-cat > $ci_source/build.sh << "EOF"
-curl http://mirrordirector.raspbian.org/raspbian.public.key  | apt-key add -
-curl http://archive.raspbian.org/raspbian.public.key  | apt-key add -
-sudo apt -q update
-
-sudo apt install devscripts equivs
-sudo mk-build-deps -ir /ci-source/build-deps/control-raspbian
-sudo apt-get -q --allow-unauthenticated install -f
-
-# Temporary fix until 3.19 is available as a pypi package
-# 3.19 is needed: https://gitlab.kitware.com/cmake/cmake/-/issues/20568
-url='https://dl.cloudsmith.io/public/alec-leamas/opencpn-plugins-stable/deb/debian'
-wget $url/pool/${OCPN_TARGET/-*/}/main/c/cm/cmake-data_3.19.3-0.1_all.deb
-wget $url/pool/${OCPN_TARGET/-*/}/main/c/cm/cmake_3.19.3-0.1_armhf.deb
-sudo apt install ./cmake_3.19.3-0.1_armhf.deb ./cmake-data_3.19.3-0.1_all.deb
-
-cd /ci-source
-rm -rf build; mkdir build; cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j $(nproc) VERBOSE=1 tarball
-ldd  app/*/lib/opencpn/*.so
-EOF
-
-
-# Run script in docker image
-#
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker run --privileged -ti \
+docker run --privileged -d -ti \
+    -e "container=docker" \
     -e "OCPN_TARGET=$OCPN_TARGET" \
     -e "CLOUDSMITH_STABLE_REPO=$CLOUDSMITH_STABLE_REPO" \
     -e "CLOUDSMITH_BETA_REPO=$CLOUDSMITH_BETA_REPO" \
     -e "CLOUDSMITH_UNSTABLE_REPO=$CLOUDSMITH_UNSTABLE_REPO" \
     -e "CIRCLE_BUILD_NUM=$CIRCLE_BUILD_NUM" \
-    -e "TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER" \
-    -v "$ci_source:/ci-source:rw" \
-    $DOCKER_IMAGE /bin/bash -xe /ci-source/build.sh
-rm -f $ci_source/build.sh
+    -v $(pwd):/ci-source:rw \
+    $DOCKER_IMAGE /bin/bash
+DOCKER_CONTAINER_ID=$(docker ps | awk '/balenalib/ {print $1}')
 
 
-# Install cloudsmith-cli (for upload) and cryptography (for git-push).
-#
+# Run build script
+cat > build.sh << "EOF"
+curl http://mirrordirector.raspbian.org/raspbian.public.key  | apt-key add -
+curl http://archive.raspbian.org/raspbian.public.key  | apt-key add -
+sudo apt -q update
+
+if [[ "$OCPN_TARGET" == stretch* ]]; then
+    cd /ci-source/build-rpi-armhf/stretch
+else
+    cd /ci-source/build-rpi-armhf/buster
+fi
+
+apt install \
+    ./libgeotiff2*_armhf.deb \
+    ./libgeotiff-dev*_armhf.deb \
+    ./libproj1*_armhf.deb \
+    ./libproj-dev_*_armhf.deb
+apt-mark hold libproj-dev libgeotiff-dev
+
+sudo apt install devscripts equivs
+sudo mk-build-deps -ir /ci-source/build-deps/control-raspbian
+sudo apt-get -q --allow-unauthenticated install -f
+
+cd /ci-source
+rm -rf build; mkdir build; cd build
+cmake -DCMAKE_BUILD_TYPE=Release  -DUSE_SYSTEM_GEOTIFF:BOOL="ON" ..
+make -j $(nproc) VERBOSE=1 tarball
+EOF
+
+docker exec -ti \
+    $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /ci-source/build.sh"
+
+
+# Stop and remove the container
+docker stop $DOCKER_CONTAINER_ID
+docker rm -v $DOCKER_CONTAINER_ID
+rm -f build.sh
+
+
+# Install cloudsmith-cli,  required by upload.sh.
 pyenv versions | sed 's/*//' | awk '{print $1}' | tail -1 \
     > $HOME/.python-version
 # Latest pip 21.0.0 is broken:
 python3 -m pip install --force-reinstall pip==20.3.4
-# https://github.com/pyca/cryptography/issues/5753 -> cryptography < 3.4
-python3 -m pip install --user cloudsmith-cli 'cryptography<3.4'
+python3 -m pip install --user cloudsmith-cli
+python3 -m pip install --user cryptography
 
-# python install scripts in ~/.local/bin, teach upload.sh to use in it's PATH:
+# python install scripts in ~/.local/bin, teach upload.sh to use it in PATH:
 echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.uploadrc
