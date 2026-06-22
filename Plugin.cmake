f0de375ed1a6150e1bca8c7569960c21751b5cf7
Rasbats <miker@members.swis.net>
2026-06-22 14:16:53 +0100

    Revert "Add gdal"
    This reverts commit f2e2304b8751730e6afedea87e5d91c773c24274.

--- Plugin.cmake
+++ Plugin.cmake
@@ -99,7 +99,4 @@ macro(add_plugin_libraries)
   add_subdirectory("libs/libgeotiff")
   target_link_libraries(${PACKAGE_NAME} ocpn::geotiff)
 
-  add_subdirectory("libs/gdal")
-  target_link_libraries(${PACKAGE_NAME} ocpn::gdal)
-
 endmacro ()
